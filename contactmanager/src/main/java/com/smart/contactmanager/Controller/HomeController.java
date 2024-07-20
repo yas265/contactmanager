965fdd650aea9de96aa6dd916e879b2f21211611
Yashvant Singh Yadav <yashawantyadav265@gmail.com>
2024-07-20 13:33:36 +0530

    Static Page structure implemented.

--- contactmanager/src/main/java/com/smart/contactmanager/Controller/HomeController.java
+++ contactmanager/src/main/java/com/smart/contactmanager/Controller/HomeController.java
@@ -3,25 +3,30 @@ package com.smart.contactmanager.Controller;
 import com.smart.contactmanager.DAO.UserRepository;
 import com.smart.contactmanager.Entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HomeController {
 
     @Autowired
     private UserRepository userRepository;
 
     @GetMapping("/home")
-    @ResponseBody
-    public String home()
+    public String home(Model model)
     {
-        User user = new User();
-        user.setName("Yashant");
-        user.setEmail("yashant2@gmail.com");
-        userRepository.save(user);
+        model.addAttribute("title", "Home - Smart Contact Manager");
+        return "Home";
+    }
 
-        return "Working";
+    @GetMapping("/about")
+    public String about(Model model)
+    {
+        model.addAttribute("title", "About - Smart Contact Manager");
+        return "About";
     }
 }
--- contactmanager/src/main/java/com/smart/contactmanager/DAO/UserRepository.java
+++ contactmanager/src/main/java/com/smart/contactmanager/DAO/UserRepository.java
@@ -1,7 +1,8 @@
 package com.smart.contactmanager.DAO;
 
 import com.smart.contactmanager.Entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
-public interface UserRepository extends CrudRepository<User, Integer> {
+public interface UserRepository extends JpaRepository<User, Integer> {
 }
